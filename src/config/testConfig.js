be4a204e407ae10e8a544bde91df08c85d35215b
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2024-04-11 14:27:30 +0200

    Prettier config added

--- src/config/testConfig.js
+++ src/config/testConfig.js
@@ -1,17 +1,17 @@
 /** @format */
 
-import "dotenv/config"
-import sequelize from "./mySql.config.js"
+import 'dotenv/config'
+import sequelize from './mySql.config.js'
 
 async function testConnection() {
-	try {
-		await sequelize.authenticate()
-		console.log("Connection has been established successfully.")
-	} catch (error) {
-		console.error("Unable to connect to the database:", error)
-	} finally {
-		await sequelize.close()
-	}
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  } finally {
+    await sequelize.close()
+  }
 }
 
 testConnection()
