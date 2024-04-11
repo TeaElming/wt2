264323cbb191eb960863b0b753c2148b545eda27
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2024-04-11 14:25:09 +0200

    Set up config for mysql using sequelize

---
+++ src/config/mySql.config.js
@@ -0,0 +1,24 @@
+/** @format */
+
+import { Sequelize } from "sequelize"
+
+// Create a new instance of Sequelize using environment variables
+const sequelize = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PASSWORD,
+	{
+		host: process.env.DB_HOST,
+		dialect: "mysql",
+		port: process.env.DB_PORT,
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000,
+		},
+		logging: false, // Set to true to see SQL queries in the console
+	}
+)
+
+export default sequelize
