0c2d2d1a7bc16fee6fce8407ea8c3968872e9a42
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2024-04-16 11:13:01 +0200

    Unnecessary console logs removed

--- src/services/educationService.js
+++ src/services/educationService.js
@@ -1,47 +1,27 @@
 import sequelize from '../config/mySql.config.js'
 
-async function fetchEducationData(geoCode) {
-  try {
-    console.log('Try to do mysql query')
-    const result = await sequelize.query(
-      `SELECT c.IMD_Decile, e.*
+export class EducationService {
+  async fetchEducationData(geoCode) {
+    try {
+      console.log('Try to do mysql query')
+      const result = await sequelize.query(
+        `SELECT c.IMD_Decile, e.*
        FROM census_2019_iod_lsoa c
        JOIN uk_lsoa_data.census_2021_education_levels_lsoa e
        ON c.LSOA21CD = e.geography_code
        WHERE c.LSOA21CD = :geoCode;`,
-      {
-        replacements: { geoCode },
-        type: sequelize.QueryTypes.SELECT,
-      }
-    )
+        {
+          replacements: { geoCode },
+          type: sequelize.QueryTypes.SELECT,
+        }
+      )
 
-    const fetchedData = result.length > 0 ? result[0] : null
+      const fetchedData = result.length > 0 ? result[0] : null
 
-    console.log('Fetched data:', fetchedData)
-    // Assuming fetchedData is the object you retrieved from the database
-    console.log('IMD Decile:', fetchedData.IMD_Decile)
-    console.log('Date:', fetchedData.date)
-    console.log('Geography:', fetchedData.geography)
-    console.log('Geography Code:', fetchedData.geography_code)
-    console.log('No Qualifications:', fetchedData.no_qualifications)
-    console.log(
-      'Level 1 Entry Qualifications:',
-      fetchedData.level_1_entry_qualifications
-    )
-    console.log('Level 2 Qualifications:', fetchedData.level_2_qualifications)
-    console.log('Apprenticeship:', fetchedData.apprenticeship)
-    console.log('Level 3 Qualifications:', fetchedData.level_3_qualifications)
-    console.log(
-      'Level 4 and Above Qualifications:',
-      fetchedData.level_4_above_qualifications
-    )
-    console.log('Other Qualifications:', fetchedData.other_qualifications)
-    console.log('Total Population:', fetchedData.total_population)
-    return fetchedData
-  } catch (error) {
-    console.error('Error fetching data:', error)
-    throw error
+      return fetchedData
+    } catch (error) {
+      console.error('Error fetching data:', error)
+      throw error
+    }
   }
 }
-
-fetchEducationData('E01000001')
--- src/services/trial.js
+++ src/services/trial.js
@@ -1,25 +1,13 @@
-import EducationService from './educationService.js';
-import { indexDataToElasticsearch, syncEvents } from './syncService.js'
+import { EducationService } from './educationService.js'
 
-// const eduService = EducationService.getInstance();
-// await eduService.getEducationLevelForArea('E01000001');
-// console.log('Education level data retrieved successfully');
-
-import sequelize from '../config/mySql.config.js'
-
-async function countEducationData() {
+async function fetchData() {
   try {
-    console.log('Try to do mysql query')
-    const result = await sequelize.query(
-      "SELECT COUNT(*) AS total_rows FROM uk_lsoa_data.census_2021_education_levels_lsoa;",
-      { type: sequelize.QueryTypes.SELECT }
-    );
-    console.log("Total rows in census_2021_education_levels_lsoa:", result[0].total_rows);
-    return result[0].total_rows;
+    const educationService = new EducationService()
+    const fetchedData = await educationService.fetchEducationData('E01000001')
+    console.log('All data: ' + JSON.stringify(fetchedData))
   } catch (error) {
-    console.error("Error fetching row count:", error);
-    throw error;
+    console.error('Error:', error)
   }
 }
 
-countEducationData();
+await fetchData()
