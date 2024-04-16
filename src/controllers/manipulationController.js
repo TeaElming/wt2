d980efc0faca0ca1b8cdab2290e2fc1b3f340e7d
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2024-04-16 15:32:56 +0200

    Incorrect routing added for testing d3

---
+++ src/controllers/manipulationController.js
@@ -0,0 +1,41 @@
+export class ManipulationController {
+  constructor(manipulationService) {
+    this.manipulationService = manipulationService
+  }
+  async manipulateGeoCodeData(req, res) {
+    try {
+      const { geoCode } = req.body // Extract geoCode from request body
+      const data = await this.manipulationService.manipulateGeoCodeData(geoCode)
+      res.status(200).json(data)
+    } catch (error) {
+      console.error('Error manipulating GeoCode data:', error)
+      res.status(500).json({ error: 'Error manipulating GeoCode data' })
+    }
+  }
+
+  async manipulateAreaData(req, res) {
+    try {
+      const { partialAreaName } = req.body // Extract partialAreaName from request body
+      const data = await this.dataManipulationService.manipulateAreaData(
+        partialAreaName
+      )
+      res.status(200).json(data)
+    } catch (error) {
+      console.error('Error manipulating area data:', error)
+      res.status(500).json({ error: 'Error manipulating area data' })
+    }
+  }
+
+  async manipulateAverageDecileEducation(req, res) {
+    try {
+      const { decile_number } = req.body // Extract decile_number from request body
+      const data = await this.dataManipulationService.averageDecileEducation(
+        decile_number
+      )
+      res.status(200).json(data)
+    } catch (error) {
+      console.error('Error averaging decile education data:', error)
+      res.status(500).json({ error: 'Error averaging decile education data' })
+    }
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { ManipulationController } from './controllers/manipulationController.js'
 
 const app = express()
 const PORT = 3000
+const manipulationController = new ManipulationController()
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-// Then we need to set up routes etc here
+// TODO: Extract into router folder, just for testing now
+app.post('/manipulate-geo-code-data', async (req, res) => {manipulationController.manipulateGeoCodeData(req, res)})
+app.post('/manipulate-area-data', async (req, res) => {manipulationController.manipulateAreaData(req, res)})
+app.post('/manipulate-average-decile-education', async (req, res) => {manipulationController.manipulateAverageDecileEducation(req, res)})
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
