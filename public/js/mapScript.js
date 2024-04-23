c5b68e43700e08e8495563227eacf910de959098
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2024-04-23 05:03:42 +0200

    Imported graph formatter to mapping script

--- public/js/mapScript.js
+++ public/js/mapScript.js
@@ -1,3 +1,6 @@
+import { GraphingFunctionality } from "./graphingFunctionality.js"
+
+const graphFormatter = new GraphingFunctionality()
 // Initialize the map
 const map = L.map('map').setView([52.461279, -1.826189], 6) // Set to England and Wales
 
@@ -78,7 +81,7 @@ function mapGeoCode(event) {
       if (data && data.percentages) {
         // Validate data structure
         console.log('Making it into the response bit? ')
-        updateGraph(formatGraphData(data))
+        graphFormatter.updateGraph(graphFormatter.formatGraphData(data))
       }
     })
     .catch((error) => {
@@ -94,104 +97,4 @@ document.addEventListener('click', function (event) {
   }
 })
 
-function formatGraphData(data) {
-  // Provide default values if certain properties are missing
-  const LSOAinfo = {
-    LSOA_name: data.LSOAinfo?.LSOA_name || '',
-    LSOA_code: data.LSOAinfo?.LSOA_code || '',
-    IMD_Decile: data.LSOAinfo?.IMD_Decile || 'N/A', // Assuming IMD_Decile might also be optional
-  }
 
-  return {
-    LSOAinfo: LSOAinfo,
-    no_qualifications: data.percentages?.no_qualifications || 0,
-    level_1_entry_qualifications:
-      data.percentages?.level_1_entry_qualifications || 0,
-    level_2_qualifications: data.percentages?.level_2_qualifications || 0,
-    apprenticeship: data.percentages?.apprenticeship || 0,
-    level_3_qualifications: data.percentages?.level_3_qualifications || 0,
-    level_4_above_qualifications:
-      data.percentages?.level_4_above_qualifications || 0,
-    other_qualifications: data.percentages?.other_qualifications || 0,
-  }
-}
-
-function updateGraph(data) {
-  const graphTitle = `${data.LSOAinfo.LSOA_name} (${data.LSOAinfo.LSOA_code}), Decile: ${data.LSOAinfo.IMD_Decile}`
-  const educationLevels = [
-    { name: 'No Qualifications', value: parseFloat(data.no_qualifications) },
-    {
-      name: 'Level 1 Entry',
-      value: parseFloat(data.level_1_entry_qualifications),
-    },
-    { name: 'Level 2', value: parseFloat(data.level_2_qualifications) },
-    { name: 'Apprenticeship', value: parseFloat(data.apprenticeship) },
-    { name: 'Level 3', value: parseFloat(data.level_3_qualifications) },
-    {
-      name: 'Level 4 and Above',
-      value: parseFloat(data.level_4_above_qualifications),
-    },
-    {
-      name: 'Other Qualifications',
-      value: parseFloat(data.other_qualifications),
-    },
-  ]
-
-  const margin = { top: 40, right: 30, bottom: 150, left: 60 },
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom
-
-  document.getElementById('graphTitle').innerText = graphTitle
-  d3.select('#graph svg').remove()
-
-  const svg = d3
-    .select('#graph')
-    .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`)
-
-  const x = d3
-    .scaleBand()
-    .range([0, width])
-    .domain(educationLevels.map((d) => d.name))
-    .padding(0.1)
-  svg
-    .append('g')
-    .attr('transform', `translate(0,${height})`)
-    .call(d3.axisBottom(x))
-    .selectAll('text')
-    .attr('transform', 'translate(-10,0)rotate(-45)')
-    .style('text-anchor', 'end')
-
-  const y = d3
-    .scaleLinear()
-    .domain([0, 1]) // This sets the scale from 0 to 1
-    .range([height, 0])
-  svg.append('g').call(d3.axisLeft(y).tickFormat(d3.format('.0%'))) // Format as percentage without decimals
-
-  svg
-    .selectAll('mybar')
-    .data(educationLevels)
-    .enter()
-    .append('rect')
-    .attr('x', (d) => x(d.name))
-    .attr('y', (d) => y(d.value))
-    .attr('width', x.bandwidth())
-    .attr('height', (d) => height - y(d.value))
-    .attr('fill', '#69b3a2')
-
-  // Adding text labels inside bars
-  svg
-    .selectAll('.text')
-    .data(educationLevels)
-    .enter()
-    .append('text')
-    .attr('class', 'label')
-    .attr('x', (d) => x(d.name) + x.bandwidth() / 2) // Center text within bars
-    .attr('y', (d) => y(d.value) + 20) // Adjust positioning to be inside the bar
-    .attr('text-anchor', 'middle')
-    .text((d) => d3.format('.0%')(d.value)) // Format the value as percentage without decimals
-    .attr('fill', 'white') // Choose a text color that contrasts with the bar color
-}
